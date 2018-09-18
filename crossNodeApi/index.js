d82df9bc97a50259448ab8fdb78f8ea39e685c68
3lv27 <elvin.gm27@gmail.com>
2018-09-18 22:33:51 +0200

    configjs + dependencies

--- crossNodeApi/index.js
+++ crossNodeApi/index.js
@@ -1,2 +1,14 @@
 'use strict'
 
+// Dependencies
+const http = require('http')
+const https = require('https')
+const url = require('url')
+const StringDecoder = require('string_decoder').StringDecoder
+const config = require('./config')
+const fs = require('fs')
+
+// Instantiate the HTTP server
+const httpServer = http.createServer((req, res) => {
+  
+})
